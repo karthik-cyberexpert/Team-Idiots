5a82c04901d4442af5f2412480406909083aadd5
[dyad] <git@dyad.sh>
2025-08-05 09:32:29 +0530

    [dyad] Fixed carousel navigation arrows visibility - wrote 1 file(s)

--- src/pages/admin/typer/ShowContentDialog.tsx
+++ src/pages/admin/typer/ShowContentDialog.tsx
@@ -38,7 +38,7 @@ export const ShowContentDialog = ({ open, onOpenChange, typerSet }: ShowContentD
               {typerSet.typing_texts.map((text, index) => (
                 <CarouselItem key={text.id}>
                   <div className="p-1">
-                    <Card className="w-[700px] h-[400px] flex flex-col">
+                    <Card className="w-[696px] h-[400px] flex flex-col">
                       <CardHeader>
                         <CardTitle>{text.title} (Text {index + 1} of 35)</CardTitle>
                       </CardHeader>
